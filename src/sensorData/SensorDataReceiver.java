7a8ee6607ae841fd85e03d90a99fadbd1b78e94c
ThoHTW <only4cs@freenet.de>
2020-04-28 22:40:02 +0200

    Challenge 4 tcp, no unit test implemented!

---
+++ src/sensorData/SensorDataReceiver.java
@@ -0,0 +1,18 @@
+package sensorData;
+
+import streamMachine.StreamMachine;
+import transmission.DataConnection;
+
+public class SensorDataReceiver {
+    private final DataConnection connection;
+    private final StreamMachine storage;
+
+    public SensorDataReceiver(DataConnection connection, StreamMachine storage) {
+        this.connection = connection;
+        this.storage = storage;
+    }
+
+    public StreamMachine getStorage() {
+        return storage;
+    }
+}
